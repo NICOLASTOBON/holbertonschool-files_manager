aeaeeedebc2884aedf1fa37b74a13b2f00c7c6c1
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2021-06-07 17:15:54 -0500

    add files

---
+++ utils/redis.js
@@ -0,0 +1,32 @@
+import redis from 'redis';
+import { promisify } from 'util';
+
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient();
+    this.client.on('error', (err) => console.log(err));
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    const getAsync = promisify(this.client.get).bind(this.client);
+    return getAsync(key);
+  }
+
+  async set(key, value, expires) {
+    const setAsync = promisify(this.client.set).bind(this.client);
+    setAsync(key, value, 'EX', expires);
+  }
+
+  async del(key) {
+    const delAsync = promisify(this.client.del).bind(this.client);
+    delAsync(key);
+  }
+}
+
+const redisClient = new RedisClient();
+
+export default redisClient;
