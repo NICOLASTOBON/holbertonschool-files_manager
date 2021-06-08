1dbe6e2065485ff7ba459499d290e000fb75a1cd
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2021-06-08 02:03:55 -0500

    add files

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,21 +1,21 @@
 import sha1 from 'sha1';
 import dbClient from '../utils/db';
 
-const postNew = async (req, res) => {
-  const { email, password } = req.body;
+class UsersController {
+  static async postNew(req, res) {
+    const { email, password } = req.body;
 
-  if (!email) return res.status(400).send('Missing email');
-  if (!password) return res.status(400).send('Missing password');
+    if (!email) return res.status(400).send('Missing email');
+    if (!password) return res.status(400).send('Missing password');
 
-  const userExist = await dbClient.users.findOne({ email });
-  if (userExist) return res.status(400).send('Already exist');
+    const userExist = await dbClient.users.findOne({ email });
+    if (userExist) return res.status(400).send('Already exist');
 
-  const pwdSha = sha1(password);
-  const { ops } = await dbClient.users.insertOne({ email, password: pwdSha });
+    const pwdSha = sha1(password);
+    const { ops } = await dbClient.users.insertOne({ email, password: pwdSha });
 
-  return res.status(201).send({ id: ops[0]._id, email: ops[0].email });
-};
+    return res.status(201).send({ id: ops[0]._id, email: ops[0].email });
+  }
+}
 
-export default {
-  postNew,
-};
+export default UsersController;
