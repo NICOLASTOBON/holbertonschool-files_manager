469c733af4a454c4b3b6cca4ce9df0b0b8000a4e
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2021-06-07 21:37:07 -0500

    add files

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ import AppController from '../controllers/AppController';
 
 const router = express.Router();
 
-router.use('/status', AppController.getStatus);
-router.use('/stats', AppController.getStats);
+router.get('/status', AppController.getStatus);
+router.get('/stats', AppController.getStats);
 
 export default router;
